7199ccce6ae1b81f9180508c5f34aa2457785ca1
eusqual <94629531+eusqual@users.noreply.github.com>
2024-05-17 03:41:19 +0100

    Add files via upload

---
+++ src/data/generated/available_img.js
@@ -0,0 +1,11 @@
+define({ availableImg: [
+    "Kim2011single",
+    "He2011single",
+    "Zhu2015a",
+    "Jobson1997a",
+    "Buades2005a",
+    "Cai2016dehazenet",
+    "Li2017aodnet",
+    "Zhang2018densely",
+    "Dosovitskiy2021an",
+    "Song2022vision"]});
